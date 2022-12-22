82b5deda0965b17715b76bb9c5324633e5aba0e1
Narek2002G <112398376+Narek2002G@users.noreply.github.com>
2022-12-23 02:11:28 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -61,3 +61,99 @@ $(window).scroll(function(){
         $('#dynamic8').removeClass('newClass8');
     }
 });
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 480) {
+        $('#dynamic9').addClass('newClass9');
+    } else {
+        $('#dynamic9').removeClass('newClass9');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 480) {
+        $('#dynamic10').addClass('newClass10');
+    } else {
+        $('#dynamic10').removeClass('newClass10');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 680) {
+        $('#dynamic11').addClass('newClass11');
+    } else {
+        $('#dynamic11').removeClass('newClass11');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 680) {
+        $('#dynamic12').addClass('newClass12');
+    } else {
+        $('#dynamic12').removeClass('newClass12');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 880) {
+        $('#dynamic13').addClass('newClass13');
+    } else {
+        $('#dynamic13').removeClass('newClass13');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 880) {
+        $('#dynamic14').addClass('newClass14');
+    } else {
+        $('#dynamic14').removeClass('newClass14');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 1080) {
+        $('#dynamic15').addClass('newClass15');
+    } else {
+        $('#dynamic15').removeClass('newClass15');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 1080) {
+        $('#dynamic16').addClass('newClass16');
+    } else {
+        $('#dynamic16').removeClass('newClass16');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 1780) {
+        $('#dynamic17').addClass('newClass17');
+    } else {
+        $('#dynamic17').removeClass('newClass17');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 1780) {
+        $('#dynamic18').addClass('newClass18');
+    } else {
+        $('#dynamic18').removeClass('newClass18');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 2180) {
+        $('#dynamic19').addClass('newClass19');
+    } else {
+        $('#dynamic19').removeClass('newClass19');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 2180) {
+        $('#dynamic20').addClass('newClass20');
+    } else {
+        $('#dynamic20').removeClass('newClass20');
+    }
+});
