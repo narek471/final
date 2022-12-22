9174e9da8aa8d150602eca3a3b9d94b34b840e86
Narek2002G <112398376+Narek2002G@users.noreply.github.com>
2022-12-22 23:19:02 +0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,63 @@
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 150) {
+       $('#dynamic').addClass('newClass');
+    } else {
+       $('#dynamic').removeClass('newClass');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 150) {
+        $('#dynamic2').addClass('newClass2');
+    } else {
+        $('#dynamic2').removeClass('newClass2');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 150) {
+        $('#dynamic3').addClass('newClass3');
+    } else {
+        $('#dynamic3').removeClass('newClass3');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 150) {
+        $('#dynamic4').addClass('newClass4');
+    } else {
+        $('#dynamic4').removeClass('newClass4');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 380) {
+        $('#dynamic5').addClass('newClass5');
+    } else {
+        $('#dynamic5').removeClass('newClass5');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 380) {
+        $('#dynamic6').addClass('newClass6');
+    } else {
+        $('#dynamic6').removeClass('newClass6');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 380) {
+        $('#dynamic7').addClass('newClass7');
+    } else {
+        $('#dynamic7').removeClass('newClass7');
+    }
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 380) {
+        $('#dynamic8').addClass('newClass8');
+    } else {
+        $('#dynamic8').removeClass('newClass8');
+    }
+});
