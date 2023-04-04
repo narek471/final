4e1a9b9973ada85b10be79b97c556e6c7c4583bf
Narek2002G <112398376+Narek2002G@users.noreply.github.com>
2023-04-04 23:31:41 +0400

    Add files via upload

--- send.php
+++ send.php
@@ -1,35 +1,12 @@
 <?php
 
-$name = $_POST["name"];
-$email = $_POST["email"];
-$subject = $_POST["subject"];
-$message = $_POST["message"];
+    $to      = 'n-ginosyan@mail.ru';
+    $subject = 'the subject';
+    $message = 'hello';
+    $headers = 'From: webmaster@example.com'       . "\r\n" .
+                 'Reply-To: webmaster@example.com' . "\r\n" .
+                 'X-Mailer: PHP/' . phpversion();
 
-require "vendor/autoload.php";
+    mail($to, $subject, $message, $headers);
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-
-$mail = new PHPMailer(true);
-
-// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
-$mail->isSMTP();
-$mail->SMTPAuth = true;
-
-$mail->Host = "smtp.example.com";
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
-
-$mail->Username = "kornenko-alex@mail.ru";
-$mail->Password = "kwork2022kwork";
-
-$mail->setFrom($email, $name);
-$mail->addAddress("n-ginosyan@mail.ru", "Dave");
-
-$mail->Subject = $subject;
-$mail->Body = $message;
-
-$mail->send();
-
-header("Location: sent.html");
+    ?>
