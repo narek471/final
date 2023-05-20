7ada2aac4243dd0c59ac5fa274349e0afe4047c6
Narek2002G <112398376+Narek2002G@users.noreply.github.com>
2023-05-20 18:53:54 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,19 +1,61 @@
-$('.gallery__body').owlCarousel({
+// OWl Carousel
+
+$('.partner__main').owlCarousel({
+    center: true,
     loop:true,
-    margin:10,
-    dots: false,
+    margin: 20,
+    dots: true,
     nav:true,
-    items:6,
-    autoplay:true
+    items:1,
+    pagination :true,
+    autoplay:false,
+    navText: ["<img src='../img/icons/arrow_left.svg'/>","<img src='../img/icons/arrow_right.svg'/>"]
 })
-//
-// const menuButton = document.querySelector('.menu-btn');
-// const menuMobile = document.querySelector('.mobile__menu__list')
-//
-// menuButton.addEventListener('click', function () {
-//     if(menuMobile.style.top === '-200px') {
-//         menuMobile.style.top = '80px';
-//     }else {
-//         menuMobile.style.top = '-200px';
-//     }
-// })
+
+// PopUps
+
+const openPupUpFormButtons = document.querySelectorAll('.open-popup-form');
+const popUpFormBg = document.querySelector('.popup__form__bg');
+const forms = document.querySelectorAll('.last__right');
+const popUpThanksBg = document.querySelector('.popup__thanks__bg');
+const closeButtons = document.querySelectorAll('.popup__close');
+
+openPupUpFormButtons.forEach(e => {
+    e.addEventListener('click', (event) => {
+        event.preventDefault();
+        if(popUpFormBg.classList.contains('active')) {
+            document.body.style.overflowY = 'auto';
+            popUpFormBg.classList.remove('active');
+        } else {
+            document.body.style.overflowY = 'hidden';
+            popUpFormBg.classList.add('active');
+        }
+    })
+})
+
+document.addEventListener('click', (e) => {
+    if(e.target === popUpFormBg || e.target === popUpThanksBg) {
+        popUpFormBg.classList.remove('active');
+        popUpThanksBg.classList.remove('active');
+        document.body.style.overflowY = 'auto';
+    }
+})
+
+forms.forEach(form => {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        popUpFormBg.classList.remove('active');
+        popUpThanksBg.classList.add('active');
+        document.body.style.overflowY = 'hidden';
+        form.reset();
+    })
+})
+
+closeButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popUpFormBg.classList.remove('active');
+        popUpThanksBg.classList.remove('active');
+        document.body.style.overflowY = 'auto';
+    })
+})
