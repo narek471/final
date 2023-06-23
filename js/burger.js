4c3c386753782c660b2d10950058e6c748ec7e24
Narek2002G <112398376+Narek2002G@users.noreply.github.com>
2023-06-23 23:49:11 +0400

    Add files via upload

--- js/burger.js
+++ js/burger.js
@@ -1,1 +1,12 @@
-const burger=document.querySelector(".burger"),modal=document.querySelector(".modal--mobmenu"),modalCloseBurger=document.querySelector(".modal__close__burger");burger.addEventListener("click",(()=>{burger.classList.toggle("burger-active"),modal.classList.toggle("modal--visible")})),modalCloseBurger.addEventListener("click",(()=>{burger.classList.toggle("burger-active"),modal.classList.toggle("modal--visible")})),document.querySelectorAll(".scroll-link").forEach((e=>{e.addEventListener("click",(()=>{modal.classList.remove("modal--visible")}))}));
+const burger = document.querySelector(".burger"), modal = document.querySelector(".modal--mobmenu"),
+    modalCloseBurger = document.querySelector(".modal__close__burger");
+burger.addEventListener("click", (() => {
+    burger.classList.toggle("burger-active"), modal.classList.toggle("modal--visible")
+})), modalCloseBurger.addEventListener("click", (() => {
+    burger.classList.toggle("burger-active"), modal.classList.toggle("modal--visible")
+})), document.querySelectorAll(".scroll-link").forEach((e => {
+    e.addEventListener("click", (() => {
+        modal.classList.remove("modal--visible");
+        burger.classList.toggle('burger-active');
+    }))
+}));
